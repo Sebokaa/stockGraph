2ac44327824a613e8fd30e60bda5c7d3e40fb8f7
Sebokaa <samuelthinks@gmail.com>
2025-01-24 03:12:10 -0500

    drag-functionality

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 export default function Graph({ data }) {
+  const [coord, setCoord] = useState([null, null]);
+  const [deltaY, setDeltaY] = useState(null)
+  const [initY, setInitY] = useState(null);
+  const chartRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    const position = event.currentTarget.getBoundingClientRect();
+    
+    const x = event.clientX - position.left;
+    const y = event.clientY - position.top;
+
+    setCoord([x, y])
+  }
+
+  const handleIsClicked = (event) => {
+    const chart = chartRef.current.chart;
+    const position = event.currentTarget.getBoundingClientRect();
+    const yInitialWindowValue = event.clientY - position.top;
+    const yInitialGraphValue = chart.yAxis[0].toValue(yInitialWindowValue);
+    // console.log(yInitialGraphValue);
+    // console.log(chart);
+    setInitY(yInitialGraphValue);
+  }
+
+  const handleIsNotClicked = async (event) => {
+    const chart = chartRef.current.chart;
+    const position = event.currentTarget.getBoundingClientRect();
+    const yFinalWindowValue = event.clientY - position.top;
+    const yFinalGraphValue = chart.yAxis[0].toValue(yFinalWindowValue);
+    deltaYCalc(yFinalGraphValue);
+  }
+
+  const deltaYCalc = (y) => {
+    const deltaYNum = y - initY;
+    setDeltaY(deltaYNum);
+  }
+
   const options = {
     chart: {
       type: "line",
@@ -31,6 +68,10 @@ export default function Graph({ data }) {
   };
   
   return (
-    <HighchartsReact highcharts={Highcharts} options={options} />
+    <div onMouseDown={handleIsClicked} onMouseUp={handleIsNotClicked} onMouseMove={handleMouseMove} style={{margin: 0, padding: 0, border: "1px solid grey", borderRadius: "5px"}}>
+    <HighchartsReact ref={chartRef} highcharts={Highcharts} options={options} />
+    <p>{coord[0]}, {coord[1]}</p>
+    <p>DeltaY: {deltaY}</p>
+    </div>
   );
 }
